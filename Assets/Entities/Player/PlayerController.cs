6cd47acd934c8db8b611c547c0b89932d11419f9
Diego Laurentino <diego.r.laurentino@gmail.com>
2016-11-02 20:24:49 -0200

    Added enemy ships, shredder, and laser beams.

---
+++ Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 15.0f;
+	public float padding = 1f;
+	public GameObject projectile;
+	public float projectileSpeed = 15f;
+	public float firingRate = 0.2f;
+
+	float xMin;
+	float xMax;
+
+	void Start () {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+		Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+		xMin = leftMost.x + padding;
+		xMax = rightMost.x - padding;
+	}
+
+	void Fire () {
+		GameObject beam = Instantiate(projectile, transform.position,Quaternion.identity) as GameObject;
+		beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0,projectileSpeed,0);
+	}
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) { 
+			InvokeRepeating ("Fire", 0.000001f, firingRate);
+		}
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			CancelInvoke("Fire");
+		}
+
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		else if(Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+
+		// restrict the player to the game space
+		float newX = Mathf.Clamp(transform.position.x, xMin, xMax);
+		transform.position = new Vector3(newX, transform.position.y,transform.position.z);
+	}
+
+
+}
